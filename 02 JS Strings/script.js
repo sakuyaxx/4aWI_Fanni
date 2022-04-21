c7bc66364545b938d58021fe46ef5a574eb8e050
sakuyaxx <fannipal8@gmail.com>
2022-04-21 09:00:54 +0200

    02 JS strings erledigt

---
+++ 02 JS Strings/script.js
@@ -0,0 +1,32 @@
+console.log("TEST");
+
+let c = 7.4;
+let d = 7.5;
+
+let result = c + d;
+console.log(result);
+
+//STRINGS
+
+let  name = "Max";
+console.log(name);
+
+console.log("Das Ergebnis von", c, "und", d, "beträgt", result);
+
+
+//Aufgaben
+
+//1) Berechne das Ergebnis von 17.4 / 3.9. Gib folgenden Satz aus: The result of 17.4 divided by 3.9 is 4.461538461538462
+
+let a = 17.4;
+let b = 3.9;
+result = a/b;
+
+console.log("The result of", a, "divided by", b, "is", result);
+
+//2) Lege eine Variable firstname und eine Varialbe lastname an. Weise den Variablen Deinen Namen zu. Gib folgendes aus: "Ich heiße VORNAME NACHNAME"
+
+let firstname = "Fanni";
+let lastname = "Pal";
+
+console.log("Ich heiße", firstname, lastname);
