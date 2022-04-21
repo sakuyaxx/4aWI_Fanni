7241988e449d97888751f90eb79363b1cfe65f9f
sakuyaxx <fannipal8@gmail.com>
2022-04-21 08:51:30 +0200

    01 JS Variablen und Rechenoperationen erledigt

---
+++ 01 JS Variablen/test.js
@@ -0,0 +1,47 @@
+console.log("you are beautiful");
+console.log("Baum");
+
+//Variablen
+
+let a = 7;
+let b = 8;
+let c = a + b;
+
+console.log("A hat den Wert:", a, "\n" + "B hat den Wert:", b);
+
+//a = 10;
+//console.log("A hat den Wert:", a);
+
+console.log("a + b =", c);
+
+
+//Rechnungsarten
+
+c = b - a;
+console.log("b - a =", c);
+
+
+//Aufgaben (berechnen und in der Console ausgeben)
+
+let result = a + b + c;
+console.log("Das Ergebnis ist", result);
+
+result = (a + b)/c;
+console.log("Das Ergebnis ist", result);
+
+result = c - a + b
+console.log("Das Ergebnis ist", result);
+
+result = a + b + c * 3
+console.log("Das Ergebnis ist", result);
+
+result = (a + b + c)/4
+console.log("Das Ergebnis ist", result);
+
+//Ändern der Variablen und die Ausgabe der neuen Werte
+
+a = 3
+b = a + 3
+c = c -3
+
+console.log("Die neuen Werte der Variablen sind", a + ",", b, "und", c)
