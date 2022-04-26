6ae71d411d190497f766ef2bd1a4c702245334c6
sakuyaxx <fannipal8@gmail.com>
2022-04-26 10:32:07 +0200

    04 JS Schleifen Aufgabe fertig

---
+++ 04 JS Schleifen/script.js
@@ -0,0 +1,47 @@
+console.log("test")
+
+let a = 6;
+let b = 4.3;
+let name = "Fanni";
+
+
+//Aufgaben
+
+// Zähle von 7 bis 45
+
+for(let num = 7; num <= 45; num++)
+{
+    console.log(num);
+}
+
+
+// Zähle von 9 bis 99
+
+for(let number = 9; number <= 99; number++)
+{
+    console.log(number);
+}
+
+
+// Zähle rückwärts von 10 bis 0
+
+for(let countBack = 10; countBack  >= 0; countBack --)
+{
+    console.log(countBack);
+}
+
+
+// Zähle rückwärts von 1234 bis 1207
+
+for(let countBack2 = 1234; countBack2  >= 1207; countBack2 --)
+{
+    console.log(countBack2);
+}
+
+
+// Gib Deinen Namen genau 8 mal aus
+
+for(let count = 0; count <= 8; count++)
+{
+    console.log(name);
+}
