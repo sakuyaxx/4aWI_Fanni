0498e7d18e7201f866e44f53d291b99d5149055c
sakuyaxx <fannipal8@gmail.com>
2022-05-18 11:10:13 +0200

    08 JS Übung fertig

---
+++ 11 JS Funktionen/script.js
@@ -0,0 +1,14 @@
+// https://www.w3schools.com/js/js_functions.asp
+
+function myFunction(p1, p2) {
+    return p1 * p2;   // The function returns the product of p1 and p2
+  };
+
+function mySecondFunction(d1, d2){
+    return d1 + d2
+};
+var x = mySecondFunction(4, 3);
+
+function toCelsius(f){
+    return (5/9) * (f-32);
+};
